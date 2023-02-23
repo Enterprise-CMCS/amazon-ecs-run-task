0d0e316994573463025ff7a297846e165783ed0a
Henry Soule <henrysoule@ip-10-232-49-219.ec2.internal>
2023-02-23 13:26:43 -0500

    add network configuration input

--- index.js
+++ index.js
@@ -88,6 +88,7 @@ async function run() {
     const taskDefinitionFile = core.getInput('task-definition', { required: true });
     const cluster = core.getInput('cluster', { required: false });
     const count = core.getInput('count', { required: true });
+    const networkConfiguration = core.getInput('network-configuration', { required: true });
     const startedBy = core.getInput('started-by', { required: false }) || agent;
     const waitForFinish = core.getInput('wait-for-finish', { required: false }) || false;
     let waitForMinutes = parseInt(core.getInput('wait-for-minutes', { required: false })) || 30;
@@ -121,6 +122,7 @@ async function run() {
       cluster: clusterName,
       taskDefinition: taskDefArn,
       count: count,
+      networkConfiguration: networkConfiguration,
       startedBy: startedBy
     })}`)
 
@@ -128,6 +130,7 @@ async function run() {
       cluster: clusterName,
       taskDefinition: taskDefArn,
       count: count,
+      networkConfiguration: networkConfiguration,
       startedBy: startedBy
     }).promise();
 
